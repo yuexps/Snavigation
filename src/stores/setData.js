83877c01f419d6bc4cad9ba457744d916db65e0d
yuexps <69074603+yuexps@users.noreply.github.com>
2024-12-09 12:14:15 +0800

    Update setData.js

--- src/stores/setData.js
+++ src/stores/setData.js
@@ -7,7 +7,7 @@ const useSetDataStore = defineStore("setData", {
       themeType: "light",
       // 壁纸类别
       // 0 本地 / 1 必应 / 2 随机风景 / 3 随机动漫 / 4 自定义
-      backgroundType: 2,
+      backgroundType: 0,
       backgroundCustom: "",
       // 壁纸遮罩
       showBackgroundGray: true,
