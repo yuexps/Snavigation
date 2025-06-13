8f74098bdc653b146216c07ec69c8b95bf0b580a
yuexps <69074603+yuexps@users.noreply.github.com>
2025-06-13 21:02:19 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,14 +20,3 @@ pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.component("SvgIcon", SvgIcon);
 app.mount("#app");
-
-// 定义全局回调函数
-window.json = function(data) {
-  console.log("JSONP response:", data);
-  // 处理返回的数据
-  if (Array.isArray(data.s)) {
-    console.log("搜索建议：", data.s);
-  } else {
-    console.warn("接口返回的数据格式不正确:", data.s);
-  }
-};
