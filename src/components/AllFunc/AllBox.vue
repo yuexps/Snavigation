45b3d0972dad9764711f62d61e84788736026c62
yuexps <69074603+yuexps@users.noreply.github.com>
2025-06-13 21:12:19 +0800

    Update AllBox.vue

--- src/components/AllFunc/AllBox.vue
+++ src/components/AllFunc/AllBox.vue
@@ -3,8 +3,8 @@
     <n-tab-pane class="no-padding height--full" name="link" tab="捷径">
       <ShortCut />
     </n-tab-pane>
-    <n-tab-pane name="note" tab="便签"> 即将完善 </n-tab-pane>
-    <n-tab-pane name="more" tab="待办"> 还能有啥呢 😢 </n-tab-pane>
+    //<n-tab-pane name="note" tab="便签"> 即将完善 </n-tab-pane>
+    //<n-tab-pane name="more" tab="待办"> 还能有啥呢 😢 </n-tab-pane>
   </n-tabs>
 </template>
 
